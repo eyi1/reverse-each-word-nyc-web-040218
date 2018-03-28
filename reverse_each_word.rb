e1d3281d583fcdd1c32bb969227862f04b4a2c13
eyi1 <ej7729@gmail.com>
2018-03-28 04:08:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,17 @@
-def reverse_each_word (string)
-string = string.split(" ")
-reversed = []
+# def reverse_each_word (string)
+# string = string.split(" ")
+# reversed = []
+# 
+# string.each do |word|
+#     reversed.unshift(word)
+#   end
+#   return reversed.join(" ")
+# end
 
-string.each do |word|
-    reversed.unshift(word)
-  end
-  return reversed.join(" ")
-end
+def first_method_reverse_each_word(array)
+  nuarray = []
+  array.map! do |element|
+    element.reverse
+  end 
+array.join(" ")
+end 
